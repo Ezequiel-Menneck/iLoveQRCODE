850db99e9d93ed2271f509d0efded62d40cead57
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-04-16 03:08:35 -0300

    featr: header component + styles

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import { HeaderArea, Logo } from "./styled";
+import { Button } from "../Button/styled";
+
+const Header = () => (
+    <HeaderArea>
+        <div className="test">
+            <Logo>
+                ssss
+            </Logo>
+            <Button>Login</Button>
+            <Button>SingUp</Button>
+        </div>
+    </HeaderArea>
+);
+
+export default Header;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HeaderArea = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 5rem;
+    background-color: blue;
+
+    .test {
+        margin: "0px auto";
+        display: "flex";
+        align-items: "center";
+    }
+`;
+
+export const Logo = styled.div`
+    padding: 0 30rem;
+    width: 50px;
+    height: 50px;
+`;
