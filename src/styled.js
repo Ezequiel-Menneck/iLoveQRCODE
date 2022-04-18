11497a01faf9c77d205389f4803be36c6d422ffc
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-04-18 00:33:08 -0300

    changing padding and new style for div

--- src/styled.js
+++ src/styled.js
@@ -3,5 +3,11 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
     width: 100%;
     height: 100%;
-    padding: 4rem;
+    padding: 1rem;
+
+    & .qr-main {
+        display: flex;
+        align-items: center;
+        width: 100%;
+    }
 `
