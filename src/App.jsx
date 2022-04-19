c484ea5ab965246e5f492dca8c47744c7ab50069
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-04-19 00:43:45 -0300

    New table for dinamic config of qr code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import "./App.css";
 import React, { useEffect, useRef, useState } from "react";
 import QRCodeStyling from "qr-code-styling";
 import Header from "./components/Header";
-import { Wrapper } from "./styled";
+import { Wrapper, Table } from "./styled";
 import { Button } from "./components/Button/styled";
 
 const qrCode = new QRCodeStyling({
@@ -25,7 +24,7 @@ const App = () => {
     const ref = useRef(null);
     const [width, setWidth] = useState(200);
     const [height, setHeight] = useState(200);
-
+    const [qrImg, setQrImg] = useState();
 
     useEffect(() => {
         qrCode.append(ref.current);
@@ -35,7 +34,7 @@ const App = () => {
         qrCode.update({
             data: url,
             width: width,
-            height: height
+            height: height,
         });
     }, [url, width, height]);
 
@@ -46,13 +45,13 @@ const App = () => {
 
     const onWidthChange = (e) => {
         e.preventDefault();
-        setWidth(e.target.value)
-    }
+        setWidth(e.target.value);
+    };
 
     const onHeightChange = (e) => {
         e.preventDefault();
-        setHeight(e.target.value)
-    }
+        setHeight(e.target.value);
+    };
 
     const onExtensionChange = (event) => {
         setFileExt(event.target.value);
@@ -69,22 +68,99 @@ const App = () => {
             <Header />
             <Wrapper>
                 <div className="qr-main">
-                    <div>
-                        <input value={url} onChange={onUrlChange} />
-                        <input type="number" value={width} min="100" max="350" onChange={onWidthChange} />
-                        <input type="number" value={height} min="100" max="350" onChange={onHeightChange} />
+                    <div className="qr-form">
+                        <Table>
+                            <tr>
+                                <th className="bbb" >Data</th>
+                                <th>
+                                    <input value={url} onChange={onUrlChange} />
+                                </th>
+                            </tr>
+                            <tr>
+                                <th className="bbb">Image</th>
+                                <th>
+                                    <input
+                                        type="file"
+                                        name="arquivos"
+                                        accept="image/png, image/jpeg"
+                                        multiple
+                                        className="input-img"
+                                    />
+                                </th>
+                            </tr>
+                            <tr>
+                                <th className="bbb">Width</th>
+                                <th>
+                                    <input
+                                        type="number"
+                                        value={width}
+                                        min="100"
+                                        max="350"
+                                        onChange={onWidthChange}
+                                    />
+                                </th>
+                            </tr>
+                            <tr>
+                                <th className="bbb">Height</th>
+                                <th>
+                                    <input
+                                        type="number"
+                                        value={height}
+                                        min="100"
+                                        max="350"
+                                        onChange={onHeightChange}
+                                    />
+                                </th>
+                            </tr>
+                        </Table>
                     </div>
-                    <div ref={ref} />
-                    <select onChange={onExtensionChange} value={fileExt}>
-                        <option value="png">PNG</option>
-                        <option value="jpeg">JPEG</option>
-                        <option value="webp">WEBP</option>
-                    </select>
-                    <Button onClick={onDownloadClick}>Download</Button>
+                    <div className="qr-display">
+                        <div ref={ref} className="qrcode" />
+                        <select onChange={onExtensionChange} value={fileExt}>
+                            <option value="png">PNG</option>
+                            <option value="jpeg">JPEG</option>
+                            <option value="webp">WEBP</option>
+                        </select>
+                        <Button onClick={onDownloadClick}>Download</Button>
+                    </div>
                 </div>
             </Wrapper>
         </>
     );
-}
+};
 
-export default App
+export default App;
+
+                        {/* <D>
+                            <label>Data</label>
+                            <input value={url} onChange={onUrlChange} />
+                        </D>
+                        <D>
+                            <label>Image</label>
+                            <input
+                                type="file"
+                                name="arquivos"
+                                accept="image/png, image/jpeg"
+                                multiple
+                            />
+                        </D>
+                        <D>
+                            <label>Width</label>
+                            <input
+                                type="number"
+                                value={width}
+                                min="100"
+                                max="350"
+                                onChange={onWidthChange}
+                            />
+                        </D>
+                        <D>
+                            <label>Height</label>
+                            <input
+                                type="number"
+                                value={height}
+                                min="100"
+                                max="350"
+                                onChange={onHeightChange}
+                            />
+                        </D> */}
