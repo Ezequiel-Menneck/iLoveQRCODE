59bb49bc837e81b2cae4d71a58d9039962fc8bd6
Ezequiel <ezequiel.menneck@salsatechnology.com>
2022-04-19 22:15:34 -0300

    improvements on style

--- src/styled.js
+++ src/styled.js
@@ -8,6 +8,7 @@ export const Wrapper = styled.section`
     & .qr-main {
         display: flex;
         align-items: center;
+        justify-content: center;
         width: 100%;
     };
 
@@ -15,10 +16,10 @@ export const Wrapper = styled.section`
         flex: 1;
         height: 100%;
         min-height: 300px;
-        width: 100%;
+        margin: 0 14rem;
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        background-color: lightgrey;
     }
 
     & .qr-display {
@@ -31,55 +32,55 @@ export const Wrapper = styled.section`
     & .qrcode {
         width: 100%;
     }
+
+    & .mainOption {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        background-color: grey;
+        padding: 0.5rem;
+        width: 100%;
+    }
+
+    & .download-button {
+        width: 6rem;
+        height: 2.5rem;
+        font-size: 16px;
+        border: none;
+    }
+
+    & .format-img {
+        width: 4rem;
+        height: 2.5rem;
+        border: none;
+        background-color: lightgray;
+    }
+
 `
 
-export const Table = styled.table`
-    & tr {
-        display: flex;
-        margin: 0 10rem;
-    }
+ export const D = styled.div`
+     display: flex;
+     align-items: center;
+     height: 100%;
+     width: 20rem;
+     margin: 0 auto;
+     padding: 0.3rem;
 
-    & th {
-        padding: 0.3rem;
-    }
+     & label {
+         width: 4rem;
+         padding: 0.2rem;
+     };
 
-    & .bbb {
-        width: 4rem;
-        display: flex;
-        justify-content: flex-start;
-    }
 
-    & th > input {
-        border: 1px solid lightgray;
-        border-radius: 2px;
-        height: 2rem;
-        padding: 0.5rem;
-    }
+     & > input {
+         border: 1px solid lightgray;
+         border-radius: 2px;
+         height: 2rem;
+         padding: 0.5rem;
+     }
 
-    & .input-img {
+     & .input-img {
         border: none;
-        padding: 0.5rem 0;
-    }
-`;
-
-// export const D = styled.div`
-//     display: flex;
-//     align-items: center;
-//     height: 100%;
-//     width: 20rem;
-//     margin: 0 auto;
-//     padding: 0.3rem;
-
-//     & label {
-//         padding: 0.2rem;
-//         margin-right: 0.3rem;
-//     };
-
-//     & > input {
-//         border: 1px solid lightgray;
-//         border-radius: 2px;
-//         height: 2rem;
-//         padding: 0.5rem;
-//     }
-
-// `;
+     }
+ `;
