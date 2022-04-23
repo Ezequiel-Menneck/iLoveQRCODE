22f999c3345084099aabd2bc8143019171ff11d3
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-04-23 11:53:16 -0300

    Styles para novos componentes

--- src/styled.js
+++ src/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.section`
     width: 100%;
@@ -9,78 +9,85 @@ export const Wrapper = styled.section`
         display: flex;
         align-items: center;
         justify-content: center;
-        width: 100%;
-    };
+    }
 
     & .qr-form {
-        flex: 1;
         height: 100%;
-        min-height: 300px;
-        margin: 0 14rem;
+        width: 30rem;
+        margin-left: auto;
         display: flex;
         flex-direction: column;
-        background-color: lightgrey;
-    }
-
-    & .qr-display {
-        flex: 1;
-        width: 100%;
-        height: 100%;
-        margin: 0 auto;
-    }
-
-    & .qrcode {
-        width: 100%;
+        background-color: #c7c6c3;
     }
 
     & .mainOption {
         display: flex;
         align-items: center;
-        width: 100%;
-        height: 100%;
-        background-color: grey;
+        height: 3rem;
+        background-color: #b5b5a5;
         padding: 0.5rem;
-        width: 100%;
+        margin-bottom: 1.2rem;
+    }
+
+    & .option {
+        display: flex;
+        align-items: center;
+        height: 3rem;
+        background-color: #b5b5a5;
+        padding: 0.5rem;
+    }
+`;
+
+export const OptionWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    width: 20rem;
+    padding: 0.5rem;
+
+    & label {
+        width: 4rem;
+        padding: 0.2rem;
+    }
+
+    & > input {
+        border: 1px solid lightgray;
+        border-radius: 2px;
+        height: 2rem;
+        padding: 0.3rem;
+    }
+
+    & .input-img {
+        border: none;
+        margin-left: 6px;
+    }
+`;
+
+export const QrWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-right: auto;
+    padding: 2rem 0 0 2rem;
+
+    & .download-main {
+        display: flex;
+        justify-content: center;
     }
 
     & .download-button {
         width: 6rem;
-        height: 2.5rem;
-        font-size: 16px;
+        height: 2.48rem;
+        font-size: 1rem;
         border: none;
+        background-color: #b5b5a5;
+        cursor: pointer;
     }
 
     & .format-img {
         width: 4rem;
         height: 2.5rem;
         border: none;
-        background-color: lightgray;
+        font-size: 1rem;
+        background-color: #b5b5a5;
     }
-
-`
-
- export const D = styled.div`
-     display: flex;
-     align-items: center;
-     height: 100%;
-     width: 20rem;
-     margin: 0 auto;
-     padding: 0.3rem;
-
-     & label {
-         width: 4rem;
-         padding: 0.2rem;
-     };
-
-
-     & > input {
-         border: 1px solid lightgray;
-         border-radius: 2px;
-         height: 2rem;
-         padding: 0.5rem;
-     }
-
-     & .input-img {
-        border: none;
-     }
- `;
+`;
